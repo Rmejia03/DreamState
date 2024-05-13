414d77d30cb57c1353e980bad2ef5e46152df644
Chayse Thenhaus <chayse_thenhaus@yahoo.com>
2024-05-13 18:05:56 -0500

    Add functionality to Enemy AI

--- UnityProject/Prototype/Assets/Scripts/enemyAI.cs
+++ UnityProject/Prototype/Assets/Scripts/enemyAI.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform shootPOS;
+    [SerializeField] GameObject bullet;
+
+    [SerializeField] int HP;
+    [SerializeField] float shootRate;
+
+    bool isShooting;
+    bool playerInRange;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //gameManager.instance.GameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(gameManager.instance.player.transform.position);
+        if (!isShooting)
+        {
+            StartCoroutine(shoot());
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet, shootPOS.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    public void takeDamage(int damage)
+    {
+        HP -= damage;
+        agent.SetDestination(gameManager.instance.player.transform.position);
+        StartCoroutine(hitFlash());
+        if (HP <= 0) 
+        {
+            //Destroy(gameObject); gameManager.instance.GameGoal(-1); 
+        }
+    }
+
+    IEnumerator hitFlash()
+    {
+        model.material.color= Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+
 }
--- UnityProject/Prototype/Assets/Scripts/gameManager.cs
+++ UnityProject/Prototype/Assets/Scripts/gameManager.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class gameManager : MonoBehaviour
 {
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+
+    public GameObject player;
+    public playerControl playerScript;
+
     // Start is called before the first frame update
     void Awake()
     {
-        
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerControl>();
     }
 
     // Update is called once per frame
@@ -17,4 +27,5 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+
 }
