c2a23a6cfc279115c94e5a088ca97ea0ad91485d
Chayse Thenhaus <chayse_thenhaus@yahoo.com>
2024-05-13 16:38:38 -0500

    Added Enemy AI and Game Manager

---
+++ UnityProject/Prototype/Assets/Scripts/gameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
