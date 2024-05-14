afdf944c6b2fd4b2e3454990920f8d00e29fabb4
Maya H <107822233+catkus12@users.noreply.github.com>
2024-05-13 23:59:10 -0500

    added pause menu, hp bar and flash

--- UnityProject/Prototype/Assets/Scripts/gameManager.cs
+++ UnityProject/Prototype/Assets/Scripts/gameManager.cs
@@ -25,7 +25,7 @@ public class gameManager : MonoBehaviour
     void Awake()
     {
         instance = this;
-        player = GameObject.FindWithTag("Player");
+        player = GameObject.FindWithTag("Player"); 
         playerScript = player.GetComponent<playerControl>();
     }
 
