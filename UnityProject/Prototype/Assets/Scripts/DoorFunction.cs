cdc7e479c1187087b70261eb46e40cc44972ca6c
Marcos Salinas <MSalinas1@student.fullsail.edu>
2024-06-15 02:54:53 -0500

    Door 100% working

--- UnityProject/Prototype/Assets/Scripts/DoorFunction.cs
+++ UnityProject/Prototype/Assets/Scripts/DoorFunction.cs
@@ -6,46 +6,71 @@ using UnityEngine.UIElements;
 public class DoorFunction : MonoBehaviour
 {
     public GameObject Instructions;
+    public GameObject CloseTxt;
     public GameObject Animation;
-    //public GameObject Trigger;
+    public GameObject Trigger;
     public AudioSource OpenSound;
     public AudioSource CloseSound;
     public bool Action = false;
+    public bool doorOpen = false;
     // Start is called before the first frame update
     void Start()
     {
         Instructions.SetActive(false);
+        CloseTxt.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
-            Instructions.SetActive(true);
+            if (doorOpen)
+            {
+                CloseTxt.SetActive(true);
+            }
+            else
+            {
+                Instructions.SetActive(true);
+            }
             Action = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Instructions.SetActive(false);
-        Action = false;
+        if (other.CompareTag("Player"))
+        {
+            Instructions.SetActive(false);
+            CloseTxt.SetActive(false);
+            Action = false;
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if(Action == true)
+            if (!doorOpen)
             {
-                Instructions.SetActive(false);
+                Trigger.SetActive(true);
+                Instructions.SetActive(true);
                 Animation.GetComponent<Animator>().Play("DoorOpen");
-                Animation.GetComponent<Animator>().Play("DoorClose");
-                //Trigger.SetActive(false);
                 OpenSound.Play();
-                CloseSound.Play();
-                Action = false;
+                doorOpen = true;
+                Instructions.SetActive(false);
+                CloseTxt.SetActive(true);
             }
+            else
+            {
+                Trigger.SetActive(true);
+                CloseTxt.SetActive(true);
+                Animation.GetComponent<Animator>().Play("DoorClose");
+                CloseSound.Play();
+                doorOpen = false;
+                CloseTxt.SetActive(false);
+                Instructions.SetActive(true);
+            }
+            Action = false;
         }
     }
 }
