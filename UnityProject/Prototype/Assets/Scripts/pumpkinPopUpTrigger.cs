b6b2b83ca97448405df6036cff5b7014b7569b1f
Rex Mejia <rrmejia@student.fullsail.edu>
2024-06-18 21:11:10 -0500

    Pumpkin can now pop out of the ground

--- UnityProject/Prototype/Assets/Scripts/pumpkinPopUpTrigger.cs
+++ UnityProject/Prototype/Assets/Scripts/pumpkinPopUpTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class pumpkinPopUpTrigger : MonoBehaviour
 {
-    public pumpkinPopUpTrigger pumpkinPopUp;
+    public enemyPopup pumpkinPopUp;
 
     private void OnTriggerEnter(Collider other)
     {
