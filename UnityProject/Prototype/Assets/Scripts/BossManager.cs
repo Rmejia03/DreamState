9d1ab70ff7474e4e512fb23a84e71e89b55778c5
Marcos Salinas <MSalinas1@student.fullsail.edu>
2024-06-23 23:59:20 -0500

    Minor Adjustments

--- UnityProject/Prototype/Assets/Scripts/BossManager.cs
+++ UnityProject/Prototype/Assets/Scripts/BossManager.cs
@@ -17,12 +17,12 @@ public class BossManager : MonoBehaviour
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else if(instance != this)
+        else if (instance != this)
         {
             Destroy(gameObject);
         }
@@ -30,34 +30,36 @@ public class BossManager : MonoBehaviour
 
     public void BossDefeated()
     {
-        defeatedBosses++;
-        CheckAllBossesDefeated();
+        defeatedBosses += 1;
         UpdateBossUI();
+        CheckAllBossesDefeated();
     }
 
     public void UpdateBossUI()
     {
-        if(defeatedBosses == 1)
+        if (defeatedBosses == 1)
         {
             boss1.SetActive(true);
         }
-        else if (defeatedBosses == 2) 
-        { 
-            boss2.SetActive(true); 
+        else if (defeatedBosses == 2)
+        {
+            boss1.SetActive(true);
+            boss2.SetActive(true);
         }
     }
 
     private void CheckAllBossesDefeated()
     {
-        if(defeatedBosses >= totalBosses)
+        if (defeatedBosses >= totalBosses)
         {
             finalPortal.SetActive(true);
+            ShowWinScreen();
         }
     }
 
     public void ShowWinScreen()
     {
-        if(winScreen != null)
+        if (winScreen != null)
         {
             winScreen.SetActive(true);
         }
